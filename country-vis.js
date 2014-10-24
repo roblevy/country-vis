4e5272c5fa716774e4aa1e30b7f29f4c3b261321
Rob Levy <robert.g.levy@gmail.com>
2014-10-24 16:13:20 +0100

    Bind centroid etc to __data__

--- country-vis.js
+++ country-vis.js
@@ -13,29 +13,35 @@ var w = 800,
     csize = 10;
 
 d3.json("maps/world-110m.json", function(e, topology) {
-    var countries = topojson
-        .feature(topology, topology.objects.countries).features,
-        countryGroups = g.selectAll("g")
-        .data(countries)
-      .enter()
-        .append("g")
-        .classed("country", true);
-    countryGroups.append("path")
-        .attr("d", path)
-        .attr("vector-effect", "non-scaling-stroke") // Stops stroke scaling with translate()
-        .attr("transform", function(d) {
-            var centroid = path.centroid(d),
-                x = centroid[0],
-                y = centroid[1],
-                /* Bounding box for scaling */
-                bounds = path.bounds(d),
-                bb_w = (bounds[1][0] - bounds[0][0]),
-                bb_h = (bounds[1][1] - bounds[0][1]),
-                /* scale-factor to ensure neither width nor height is
-                 * greater than csize */
-                scale = bb_w > bb_h ? csize / bb_w : csize / bb_h;
-            return "translate(" + x + "," + y + ")"
-                + "scale(" + scale + ")"
-                + "translate(" + -x + "," + -y + ")";
-        });
+  var countries = topojson
+    .feature(topology, topology.objects.countries).features,
+  countryGroups = g.selectAll("g")
+    .data(countries)
+    .enter()
+    .append("g")
+    .classed("country", true);
+  // calculate centroids
+  countries.map(function(d) {
+    d.centroid = path.centroid(d);
+    d.x = d.centroid[0];
+    d.y = d.centroid[1];
+    d.bb = path.bounds(d);
+    d.bb_w = (d.bb[1][0] - d.bb[0][0]),
+    d.bb_h = (d.bb[1][1] - d.bb[0][1]);
+    return d;
+  });
+  countryGroups.append("path")
+    .attr("d", path)
+    .attr("vector-effect", "non-scaling-stroke") // Stops stroke scaling with translate()
+    .attr("transform", function(d) {
+      /* scale-factor to ensure neither width nor height is
+      * greater than csize */
+      var scale = d.bb_w > d.bb_h ? csize / d.bb_w : csize / d.bb_h;
+      return "translate(" + d.x + "," + d.y + ")"
+        + "scale(" + scale + ")"
+        + "translate(" + -d.x + "," + -d.y + ")";
+    });
+  countryGroups.data(function(d) {
+    return path.centroid(d);
+  }).append("rect")
 });
